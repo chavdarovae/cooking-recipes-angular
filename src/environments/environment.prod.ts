1e57ca6ec4e15be12a206a4eaadb97a102c18a76
Emiliya Chavdarova <emiliya@carrypicker.com>
2025-12-15 22:59:56 +0100

    Adjusted backend url for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     envName: 'prod',
     host: 'http://localhost:4200',
     production: true,
-    backendUrl: 'https://cooking-recipes-restful-express.onrender.com',
+    backendUrl: 'https://cooking-recipes-nestjs.onrender.com',
 };

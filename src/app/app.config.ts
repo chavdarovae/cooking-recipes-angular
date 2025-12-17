f258f481914f076fef331d76825a2bf9491c6aff
Emiliya Chavdarova <emiliya@carrypicker.com>
2025-12-17 09:23:55 +0100

    adds withHashLocation()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import {
     provideRouter,
     RouterModule,
     withComponentInputBinding,
+    withHashLocation,
 } from '@angular/router';
 
 import {
@@ -36,7 +37,7 @@ export const appConfig: ApplicationConfig = {
                 useHash: true,
             }),
         ),
-        provideRouter(routes, withComponentInputBinding()),
+        provideRouter(routes, withComponentInputBinding(), withHashLocation()),
         provideHttpClient(),
         provideHttpClient(withInterceptorsFromDi()),
         {
